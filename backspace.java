54cb2062c6c36e15dfdf84111b627fd2a42f4252
Mike Menendez <mmenendez@csumb.edu>
2019-04-15 15:21:22 -0700

    Create backspace.java

---
+++ backspace.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class backspace {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String in = scan.nextLine();
+        int counter = 0;
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < in.length(); i++) {
+            while (counter> 0){
+                out.deleteCharAt(out.length()-1);
+                counter--;
+            }
+            if(in.charAt(i) == '<'){
+                counter++;
+            }else{
+                out.append(in.charAt(i));
+            }
+        }
+        if(counter >0){
+            while (counter > 0){
+                out.deleteCharAt(out.length()-1);
+                counter--;
+            }
+        }
+        System.out.print(out);
+
+    }
+}
