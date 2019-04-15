8b171993dce2c102a88a7b4fb1e860a75c517b1a
Mike Menendez <mmenendez@csumb.edu>
2019-04-15 15:40:31 -0700

    Create aaah.java

---
+++ aaah.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class aaah {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        System.out.print(a.length() >= b.length() ? "go" : "no");
+    }
+}
