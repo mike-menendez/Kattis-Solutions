48b0e5360faffc2acf32b040626b0a60a046e133
Mike Menendez <mmenendez@csumb.edu>
2019-04-15 15:22:03 -0700

    Update backspace.java

--- backspace.java
+++ backspace.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class backspace {
     public static void main(String[] args) {
@@ -7,23 +7,22 @@ public class backspace {
         int counter = 0;
         StringBuilder out = new StringBuilder();
         for (int i = 0; i < in.length(); i++) {
-            while (counter> 0){
-                out.deleteCharAt(out.length()-1);
+            while (counter > 0) {
+                out.deleteCharAt(out.length() - 1);
                 counter--;
             }
-            if(in.charAt(i) == '<'){
+            if (in.charAt(i) == '<') {
                 counter++;
-            }else{
+            } else {
                 out.append(in.charAt(i));
             }
         }
-        if(counter >0){
-            while (counter > 0){
-                out.deleteCharAt(out.length()-1);
+        if (counter > 0) {
+            while (counter > 0) {
+                out.deleteCharAt(out.length() - 1);
                 counter--;
             }
         }
         System.out.print(out);
-
     }
 }
